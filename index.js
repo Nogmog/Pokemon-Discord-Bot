d6833cb42996ea5b97e9eb7c71c55fc13aa4ef76
seanjoe02 <seanjoe02>
2023-02-24 13:32:26 +0000

    Now announces what pokemon has spawned with a timer that does nothing right now

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const { Attachment, Message, MessageEmbed, Discord } = require("discord.js");
 const Client = require('./Client');
 const client = new Client({ intents: [3243773] });
-let pokemon = [ "./pokemon/pikachu.jpg", "./pokemon/eevee.jpg", "./pokemon/charmander.jpg", "./pokemon/squirtle.jpg" ]
+let pokemon = [ "Pikachu", "Eevee", "Charmander", "Squirtle" ]
+let currentPokemon = null;
+let timer = 60;
 
 
 client.once('ready', async () => {
@@ -15,7 +17,9 @@ client.on("messageCreate", message => {
   var msg = message.content;
 
   if (msg === prefix + 'pokemon') {let selected = Math.floor(Math.random() * pokemon.length);
-    message.channel.send( /*pokemon[selected]*/{files: [pokemon[selected]] });
+    currentPokemon = pokemon[selected];
+    message.channel.send({ files: ["./pokemon/" + currentPokemon + ".jpg"] });
+    message.channel.send("A wild " + currentPokemon + " has spawned! You have " + timer + " seconds to catch it!");
 
   }
   if (message.content === "ping"){
